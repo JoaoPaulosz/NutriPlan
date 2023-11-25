fe88093586e5b02d6050a0fb5d488353d9176eec
João Souza <joao.paulo.de.souza15t@gmail.com>
2023-11-25 17:21:45 -0300

    Criado App

---
+++ js/cadastroUsuario.js
@@ -0,0 +1,14 @@
+const emailCadastro = document.getElementById('email');
+const nomeCadastro = document.getElementById('nome');
+const senhaCadastro = document.getElementById('senha');
+
+
+const btnCadastrar = document.getElementById('btnCadastrar')
+btnCadastrar.addEventListener('click', () => {
+    cadastrarUsuario();
+})
+
+
+async function cadastrarUsuario(){
+
+}
---
+++ js/conexaoApi.js
@@ -0,0 +1,18 @@
+async function cadastroUsuario(emailCadastro, senhaCadastro, nomeCadastro){
+    var formdata = new FormData();
+    formdata.append("usuario_email",emailCadastro.value);
+    formdata.append("usuario_senha",senhaCadastro.value);
+    formdata.append("usuario_nome", nomeCadastro.value);
+    
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+      mode:'no-cors'
+    };
+    
+    const conexao = fetch("https://localhost:7256/Usuario", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
